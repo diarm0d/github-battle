dec5eb7d7eeeff1ac564ee72b1cd243420ee3cbd
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2022-09-06 14:36:03 +0200

    first commit github battle

---
+++ app/components/Popular.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Popular extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedLanguage: "All"
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage,
+        });
+    }
+  render() {
+      const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+    return (
+    <main>
+      <select onChange={(e) => this.updateLanguage(e.target.value)} selected={this.state.selectedLanguage}>
+          {languages.map((language)=> (
+            <option key={language} value={language}>{language}</option>
+          ))}      
+      </select>
+      {JSON.stringify(this.state, null, 2)}
+    </main>
+    )
+  }
+}
---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Popular from "./components/Popular";
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className="light">
+            <div className="container">
+                <Popular />
+            </div>
+        </div>
+        );
+  }
+}
+
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
